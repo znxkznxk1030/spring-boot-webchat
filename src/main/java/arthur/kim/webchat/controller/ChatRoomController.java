e27b63a33c2fc81abe9195a917a25ec1d8c72ac6
ysKim <znxkznxk1030@gmail.com>
2022-04-08 16:46:24 +0900

    [ ADD ] "/chat/room" for creating a chatroom

--- src/main/java/arthur/kim/webchat/controller/ChatRoomController.java
+++ src/main/java/arthur/kim/webchat/controller/ChatRoomController.java
@@ -5,10 +5,7 @@ import arthur.kim.webchat.registry.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,13 +27,19 @@ public class ChatRoomController {
         return chatRoomRepository.findAllRoom();
     }
 
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomRepository.createChatRoom(name);
+    }
+
     @GetMapping("/room/enter/{roomId}")
     public String roomDetail(Model model, @PathVariable String roomId) {
         model.addAttribute("roomId", roomId);
         return "/chat/roomdetail";
     }
 
-    @GetMapping
+    @GetMapping("/room/{roomId}")
     @ResponseBody
     public ChatRoom roomInfo(@PathVariable String roomId) {
         return chatRoomRepository.findRoomById(roomId);
