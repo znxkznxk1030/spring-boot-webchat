d61a99d0d7ca2d43f43c6cb8f6c15e26adc1414f
ysKim <znxkznxk1030@gmail.com>
2022-06-10 22:34:34 +0530

    [ add ] redis 설정

---
+++ src/main/java/arthur/kim/webchat/config/EmbeddedRedisConfig.java
@@ -0,0 +1,36 @@
+package arthur.kim.webchat.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * 채팅 서버가 실행될때 Embeded Redis 서버도 동시에 실행 될 수 있도록 설정
+ * - local 환경에서만 실행
+ */
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void setRedisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
